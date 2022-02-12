d0a50c8ebdeb8f216ccf8fd35ac8e043412e617a
Igor <houwat@gmail.com>
2022-02-12 13:13:56 -0500

    add more tests for add()

--- spec/practice/PracticeSpec.js
+++ spec/practice/PracticeSpec.js
@@ -7,15 +7,28 @@ describe("Practice", function() {
   })
 
   // Equality
-  it("should be able to add to integers", function() {
+  it("should be able to add two integers", function() {
     expect(practice.add(1, 2)).toEqual(3);
+  });
+
+  it("should be able to add floating-point numbers", function() {
+    expect(practice.add(1.5, 2.0)).toEqual(3.5);
+  });
+
+  it("should be able to add an integer and a floating-point number", function() {
+    expect(practice.add(1.5, 2)).toEqual(3.5);
+  });
+
+  
+  it("should be able to add a negative number", function() {
+    expect(practice.add(-1.5, 1.5)).toEqual(0);
   });
 
   // Error handling
   it("should throw an error if arguments are not of type number", function() {
     expect(function() {
       practice.add(1, '3');
-    }).toThrowError("This function accepts integers only.");
+    }).toThrowError("This function accepts arguments of type number only.");
   });
 
   describe("Testing Spies", function() {
